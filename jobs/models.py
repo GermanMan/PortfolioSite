c30167e18f86f1cd67a9c0aa3769aaf17b320805
GermanMan <adammstrotman@gmail.com>
2021-01-13 19:22:08 -0500

    Added Blog and Projects Pages

--- jobs/models.py
+++ jobs/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Job(models.Model):
      image = models.ImageField(upload_to='images/')
-     text = models.CharField(max_length=200)
+     text = models.CharField(max_length=10000)
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Job
+
 # Create your views here.
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html',{'jobs':jobs})
+
+def projects(request):
+    projects = Job.objects
+    return render(request, 'jobs/projects.html',{'jobs':projects})
